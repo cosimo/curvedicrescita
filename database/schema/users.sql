44d485a6a7cb62f54283bdb4100b9fd150104959
Cosimo Streppone <cosimo@cpan.org>
2009-01-24 16:06:41 +0000

    Extended users schema with pregnancy and children information
    git-svn-id: svn://localhost/curvedicrescita.com/trunk@98 a22adaaf-26af-384a-bbed-527b507455b5

--- database/schema/users.sql
+++ database/schema/users.sql
@@ -6,10 +6,26 @@
 
 /*use opera;*/
 
-insert into users (username, realname, password, isadmin, createdon, language, lastlogon ) values
-    ('cosimo', 'Cosimo Streppone',  sha1('cosimo'),  true,  now(), 'it', NULL),
-    ('edoardo','Edoardo Sabadelli', sha1('edoardo'), false, now(), 'en', NULL),
-    ('vetler', 'Vetle Roeim',       sha1('vetler'),  true,  now(), 'en', NULL),
-    ('ftotti', 'Francesco Totti',   sha1('chanel'),  false, now(), 'it', NULL),
-    ('vrossi', 'Valentino Rossi',   sha1('vrossi'),  false, now(), 'it', NULL);
+BEGIN WORK;
+
+CREATE TABLE users (
+    username  char(30) not null,
+    realname  varchar(60) not null,
+    password  char(40) not null,
+    isadmin   int not null default '0',
+    createdon timestamp not null default current_timestamp,
+    lastlogon timestamp,
+    language  char(12) not null default 'it_IT',
+    gender    char(1) not null default 'f',
+    pregnancy char(1) not null default '0',
+    children  int not null default 0,
+    memo      varchar(200) default '',
+    primary key (username)
+);
+
+INSERT INTO "users" VALUES('tamara','Tamara De Zotti','V8GDJBECK',1,'2008-11-08 11:18:36','2009-01-16 09:58:04','it','f','4',2);
+INSERT INTO "users" VALUES('cosimo','Cosimo Streppone','TecLS29Neo',1,'2008-11-08 11:18:58','2009-01-15 08:18:00','en','m','0',2);
+INSERT INTO "users" VALUES('antonietta','Antonietta Faino','baccellina',0,'2008-11-08 14:44:35','2009-01-09 20:54:01','it_IT','f','4',2);
+
+COMMIT;
 
