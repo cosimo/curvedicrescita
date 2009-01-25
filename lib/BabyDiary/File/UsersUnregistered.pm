98031cb1e92425f4107648066245678be9fab69e
Cosimo Streppone <cosimo@cpan.org>
2009-01-25 12:40:21 +0000

    Users unreg
    git-svn-id: svn://localhost/curvedicrescita.com/trunk@106 a22adaaf-26af-384a-bbed-527b507455b5

---
+++ lib/BabyDiary/File/UsersUnregistered.pm
@@ -0,0 +1,13 @@
+# $Id: Users.pm 32 2008-10-26 06:58:24Z Cosimo $
+
+package BabyDiary::File::UsersUnregistered;
+
+use strict;
+use base qw(BabyDiary::File::Users);
+
+use constant TABLE  => 'users_unregistered';
+
+1;
+
+#
+# End of class
