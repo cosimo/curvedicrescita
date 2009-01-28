e8bafd4bc942833959e06c589a30fa0cf143b76d
Cosimo Streppone <cosimo@cpan.org>
2009-01-28 21:30:57 +0000

    Fixed encoding issues with RSS feed
    git-svn-id: svn://localhost/curvedicrescita.com/trunk@135 a22adaaf-26af-384a-bbed-527b507455b5

--- lib/BabyDiary/Application/RSS.pm
+++ lib/BabyDiary/Application/RSS.pm
@@ -3,8 +3,8 @@
 package BabyDiary::Application::RSS;
 
 use strict;
+use Encode;
 use XML::RSS;
-
 use BabyDiary::File::Articles;
 
 sub articles {
@@ -15,7 +15,7 @@ sub articles {
         $how_many = 25; 
     }
 
-    my $rss = XML::RSS->new( version => '1.0' );
+    my $rss = XML::RSS->new(version => '1.0'); #, encoding=>'ISO-8859-15');
 
     $rss->channel(
         title => 'CurveDiCrescita.com - Ultimi articoli',
@@ -39,7 +39,7 @@ sub articles {
         $rss->add_item(
             title => $item->{title},
             link  => $host . '/exec/home/article/?id=' . $item->{id},
-            description => $item->{content},
+            description => decode('utf-8', $item->{content}),
         );
     }
 
