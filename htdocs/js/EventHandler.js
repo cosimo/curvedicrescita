52e9eaffed8d8d3e57fbe622dcf32037a60f49c0
Cosimo Streppone <cosimo@cpan.org>
2009-01-31 19:57:44 +0000

    Added event handling code
    git-svn-id: svn://localhost/curvedicrescita.com/trunk@146 a22adaaf-26af-384a-bbed-527b507455b5

---
+++ htdocs/js/EventHandler.js
@@ -0,0 +1,14 @@
+// Cross browser event handling
+// $Id$
+function add_event (evt, cb, el) {
+    if (! el) el = document;
+    if (typeof el == 'string') el = document.getElementById(el);
+    if (el.attachEvent) {
+        evt = 'on' + evt;
+        el.attachEvent(evt, cb);
+    }
+    else if (el.addEventListener) {
+        el.addEventListener(evt, cb, false);
+    }
+}
+
