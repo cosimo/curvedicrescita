d4bbd1858a151e88af7a5c46bb55cb63595292eb
Cosimo Streppone <cosimo@cpan.org>
2009-06-21 09:48:49 +0000

    Added comments table schema
    git-svn-id: svn://localhost/curvedicrescita.com/trunk@247 a22adaaf-26af-384a-bbed-527b507455b5

---
+++ database/schema/comments.sql
@@ -0,0 +1,23 @@
+/* $Id:$ */
+
+BEGIN;
+
+CREATE TABLE comments (
+	id integer primary key,
+	type char(1) not null,      /* Type of comment? */
+	rtype char(3) not null,     /* Comment to what? "art", "..." ? Article? */
+	rid integer not null,       /* Which article? articles.id */
+	content text,
+	createdby char(30) not null,
+	createdon timestamp not null default current_timestamp,
+	lastupdateby char(30),
+	lastupdateon timestamp,
+	keywords text,
+	-- 0:non public, 1:public
+	published integer not null default 1
+);
+
+CREATE INDEX i_comments_related ON comments (rtype, rid);
+
+COMMIT;
+
