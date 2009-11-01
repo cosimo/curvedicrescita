293f552a26ee6943b7665f961cab1a8f45c01df2
Cosimo Streppone <cosimo@cpan.org>
2009-11-01 15:20:28 +0000

    Refined questions schema and added answers
    git-svn-id: svn://localhost/curvedicrescita.com/trunk@295 a22adaaf-26af-384a-bbed-527b507455b5

---
+++ database/schema/answers.sql
@@ -0,0 +1,25 @@
+/* $Id$ */
+
+BEGIN;
+
+CREATE TABLE answers (
+	id integer primary key,
+	type char(1) not null,      /* Type of question? */
+	rtype char(3) not null default 'que',
+	rid integer not null,       /* Which question does this answer refer to? */
+	content text,
+	createdby char(100) not null,
+	createdon timestamp not null default current_timestamp,
+	lastupdateby char(100),
+	lastupdateon timestamp,
+	keywords text,
+	-- 0:non public, 1:public
+	published integer not null default 1,
+	reputation integer not null default 0,
+	modified timestamp not null
+);
+
+CREATE INDEX i_answers_question ON answers (rid);
+
+COMMIT;
+
--- database/schema/questions.sql
+++ database/schema/questions.sql
@@ -22,6 +22,8 @@ CREATE TABLE questions (
 	published integer not null default 0,
 	views     integer not null default 0,
 	favorited integer not null default 0,
+	reputation integer not null default 0,
+	answers   integer not null default 0,
 	modified  timestamp not null
 );
 
